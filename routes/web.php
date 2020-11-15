ae63d41815c60ea295e95abc403f6b90430c94ac
kuro.shun <black.huke9630@gmail.com>
2020-11-15 16:39:16 +0000

    user

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,27 @@ Route::resource('tasks', 'TasksController');
 
 Route::get('tasks/{id}/copy', 'TasksController@copy');
 
+
+
+/*　ユーザー登録機能
+*/
+        //　ユーザー登録ページ取得
+        Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+        //　ユーザー登録を処理
+        Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+
+
+
+/*　ログイン機能
+    ->name()は、formやlink_to_route()で使う
+*/
+
+        //　ログインページ取得
+        Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+        
+        //　ログイン情報を処理
+        Route::post('login', 'Auth\LoginController@login')->name('login.post');
+        
+        //　
+        Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
